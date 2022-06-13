dc8869575e03320bced93d56c07e1b2c129c79be
Christophe Loeys <christophe.loeys@allphi.eu>
2022-06-13 16:33:40 +0200

    added tests

--- sample/quickstart.js
+++ sample/quickstart.js
@@ -6,38 +6,36 @@ const LeagueJs = require('../lib/LeagueJS.js');
 const api = new LeagueJs(testConfig.API_KEY);
 
 
-// api.Summoner
-// 	.gettingByName(testConfig.summoner.name)
-// 	.then(data => {
-// 		'use strict';
-// 		console.log(data);
-// 	})
-// 	.catch(err => {
-// 		'use strict';
-// 		console.log(err);
-// 	});
+api.Summoner
+	.gettingByName(testConfig.summoner.name)
+	.then(data => {
+		'use strict';
+		console.log(data);
+	})
+	.catch(err => {
+		'use strict';
+		console.log(err);
+	});
 
-// api.Summoner
-// 	.gettingByAccount(testConfig.summoner.accountIdV4, 'euw')
-// 	.then(data => {
-// 		'use strict';
-// 		console.log(data);
-// 	})
-// 	.catch(err => {
-// 		'use strict';
-// 		console.log(err);
-// 	});
+api.Summoner
+	.gettingByAccount(testConfig.summoner.accountIdV4, 'euw')
+	.then(data => {
+		'use strict';
+		console.log(data);
+	})
+	.catch(err => {
+		'use strict';
+		console.log(err);
+	});
 
-// api.Challenges
-// 	.gettingChallengesConfig()
-// 	.then(data => {
-// 		'use strict';
-// 		console.log(data);
-// 	})
-// 	.catch(err => {
-// 		'use strict';
-// 		console.log(err);
-// 	});
-
-	api.Challenges.gettingLeaderboardsByLevelForChallenge("CHALLENGER", 101101).then(data => console.log(data));
+api.Challenges
+	.gettingChallengesConfig()
+	.then(data => {
+		'use strict';
+		console.log(data);
+	})
+	.catch(err => {
+		'use strict';
+		console.log(err);
+	});
 
--- test/endpoints/ChallengesEndpoint.test.js
+++ test/endpoints/ChallengesEndpoint.test.js
@@ -20,9 +20,113 @@ describe('ChallengesEndpoint Testsuite', function () {
 		endpoint = new ChallengesEndpoint(mergedConfig, TestUtil.createRateLimiter(per10, per600, allowBursts));
 	});
 
-	it('has its name added to default retryEndpoints', function () {
+	it('has its name added to default retryEndpoints', () => {
 		endpoint.config.limits.retryEndpoints.should.include(endpoint.name);
 	});
 
-	// TODO
+	describe('gettingLeaderboardsByLevelForChallenge', () =>  {
+		it('can request the leaderboards for a challenge with a valid level and challengeId', () => {
+			const promise = endpoint.gettingLeaderboardsByLevelForChallenge("CHALLENGER", mock_summoner.challengeId);
+
+			return Promise.all([
+				promise.should.eventually.be.an('Array'),
+				promise.should.eventually.have.lengthOf(200),
+				promise.then((data) => {
+					d = data[0];
+					return Promise.all([
+						d.should.have.property('position').a('number'),
+						d.should.have.property('puuid').a('string'),
+						d.should.have.property('value').a('number')
+					]);
+				})
+			]);
+		});
+
+		it('can request the leaderboards with a specific limit', () => {
+			return endpoint.gettingLeaderboardsByLevelForChallenge("CHALLENGER", mock_summoner.challengeId, { limit: 10 })
+				.should.eventually.have.lengthOf(10);
+		});
+
+		it('can request the leaderboards with a max of 1000', () => {
+			return endpoint.gettingLeaderboardsByLevelForChallenge("CHALLENGER", mock_summoner.challengeId, { limit: 2000 })
+				.should.eventually.have.lengthOf(1000);
+		});
+
+		it('fails when providing a wrong level', () => {
+			const validLevels = ["CHALLENGER", "MASTER", "GRANDMASTER"];
+			const invalidLevel = "DIAMOND";
+			return endpoint.gettingLeaderboardsByLevelForChallenge(invalidLevel, mock_summoner.challengeId)
+				.should.eventually.be.rejectedWith(`level has to be one of ${validLevels}. Received: ${invalidLevel}`);
+		});
+
+		it('fails when challengeId is invalid', () => {
+			const invalidChallengeId = "Invalid";
+			return endpoint.gettingLeaderboardsByLevelForChallenge(invalidLevel, invalidChallengeId)
+				.should.eventually.be.rejectedWith(`${challengeId} has to be a number or numerical string. Received: ${invalidChallengeId}`);
+		});
+	});
+
+	describe('gettingChallengesConfig', () => {
+		it('returns the config for all challenges', () => {
+			const promise =  endpoint.gettingChallengesConfig();
+			
+			return Promise.all([
+				promise.should.eventually.be.an('Array'),
+				promise.should.eventually.not.be.empty()
+			]);
+		});
+	});
+
+	describe('gettingChallengeConfigById', () => {
+		it('returns the config for the requested challenge', () => {
+			const promise = endpoint.gettingChallengeConfigById(mock_summoner.challengeId);
+
+			return Promise.all([
+				promise.should.eventually.have.property('id').a('number'),
+				promise.should.eventually.have.property('localizedNames').an('object'),
+				promise.should.eventually.have.property('state').a('string'),
+				promise.should.eventually.have.property('leaderboard').a('boolean'),
+				promise.should.eventually.have.property('thresholds').an('object'),
+			]);
+		});
+	});
+
+	describe('gettingChallengePercentiles', () => {
+		it('returns the percentiles per level for all challenges', () => {
+			return endpoint.gettingChallengesPercentiles()
+				.should.eventually.be.an('object');
+		});
+	});
+
+	describe('gettingChallengePercentilesById', () => {
+		it('returns the percentiles per level for a specific challenges', () => {
+			const promise = endpoint.gettingChallengePercentilesById(mock_summoner.challengeId);
+
+			return Promise.all([
+				promise.should.eventually.have.property('NONE').a('number'),
+				promise.should.eventually.have.property('IRON').a('number'),
+				promise.should.eventually.have.property('BRONZE').a('number'),
+				promise.should.eventually.have.property('SILVER').a('number'),
+				promise.should.eventually.have.property('GOLD').a('number'),
+				promise.should.eventually.have.property('PLATINUM').a('number'),
+				promise.should.eventually.have.property('DIAMOND').a('number'),
+				promise.should.eventually.have.property('MASTER').a('number'),
+				promise.should.eventually.have.property('GRANDMASTER').a('number'),
+				promise.should.eventually.have.property('CHALLENGER').a('number')
+			]);
+		});
+	});
+
+	describe('gettingChallengesPlayerData', () => {
+		it('returns challenge statistics for a specific player', () => {
+			const promise = endpoint.gettingChallengePercentilesById(mock_summoner.challengeId);
+
+			return Promise.all([
+				promise.should.eventually.have.property('totalPoints').an('object'),
+				promise.should.eventually.have.property('categoryPoints').an('object'),
+				promise.should.eventually.have.property('challenges').an('array'),
+				promise.should.eventually.have.property('preferences').an('object')
+			]);
+		});
+	});
 });
